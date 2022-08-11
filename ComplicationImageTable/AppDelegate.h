ac7039ca279929cd14b099fbe924756c2b881297
Lee Ann Rucker <lrucker@mac.com>
2022-08-10 23:10:07 -0700

    Initial Commit

---
+++ ComplicationImageTable/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  ComplicationImageTable
+//
+//  Created by Lee Ann Rucker on 8/10/22.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ ComplicationImageTable/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  ComplicationImageTable
+//
+//  Created by Lee Ann Rucker on 8/10/22.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
